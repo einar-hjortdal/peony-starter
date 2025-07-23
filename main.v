232b8012e49f1195f9ad33e4dfa1d73356941d5b
Einar Hjortdal <einar.hjortdal@outlook.com>
2025-07-23 11:50:24 +0200

    Config blobly

--- main.v
+++ main.v
@@ -1,9 +1,19 @@
 module main
 
 import einar_hjortdal.peony
+import os
+
+const env_blobly_url = 'BLOBLY_URL'
+const env_blobly_secret_key = 'BLOBLY_SECRET_KEYS'
+const env_blobly_access_key = 'BLOBLY_ACCESS_KEYS'
 
 fn main() {
-	bp := peony.new_provider_blob_blobly('', '')
+	blobly_url := os.getenv(env_blobly_url)
+	blobly_secret_keys := os.getenv(env_blobly_secret_key).split(',')
+	blobly_access_keys := os.getenv(env_blobly_access_key).split(',')
+
+	bp := peony.new_provider_blob_blobly(blobly_url, blobly_access_keys, blobly_secret_keys)
+
 	mut app := peony.new_peony_app(bp)
 	app.run()
 }
